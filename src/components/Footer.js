c42cd6d4a01e5c2019f8969ebfbb9bb1b53b4317
Christian <christianmacarthur17@gmail.com>
2023-07-10 21:47:22 +0200

    .

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+function Footer() {
+  return (
+    <>
+      {/* <div className="flex justify-center"> */}
+      <div className="absolute bottom-4 left-4">
+        <h1 className="mix-blend-overlay text-white">
+        © Christian MacArthur, 2023
+        </h1>
+      </div>
+      {/* </div> */}
+    </>
+  );
+}
+
+export default Footer;
