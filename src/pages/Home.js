22b6e7a7c2bf3fffc4be83179ca7b2395bf9538e
Christian <christianmacarthur17@gmail.com>
2024-01-30 07:54:59 +0200

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,75 +13,23 @@ import downArrow from "../assets/icons/chevron-down.svg";
 import Footer from "../components/Footer.js";
 
 const Home = () => {
-  // const [playing, setPlaying] = useState(false);
-
-  // const handleValueChange = (playing) => {
-  //   setPlaying(playing);
-  // };
-  // const router = useRouter();
-
   return (
     <>
-      {/* <Cursor isVisible={false} /> */}
-
-      <div className="relative h-screen overflow-hidden ">
+      <div className="relative h-screen overflow-y-auto overflow-x-hidden ">
         <div className="absolute top-0 left-0 w-full h-full">
           <Background />
         </div>
-
-        {/* <Navbar type="home" name="CHRISTIAN" router={router} /> */}
-        <Navbar type="home" name="CHRISTIAN" />
+        <Navbar type="home" />
         <main className="relative z-10 flex flex-col items-center justify-center h-3/4 sm:h-full">
-          <div className="flex flex-row align-center items-center justify-center gap-8 h-1/2">
-            {/* <div className="glass-frame">
-              <h1
-                // onMouseEnter={textEnter}
-                // onMouseLeave={textLeave}
-                className="glass-text-main duration-500 transition-all"
-              >
-                Christian MacArthur
-              </h1>
-            </div> */}
-            <center>
-              <h1 className="text-white text-5xl w-[1000px]">
-                Software Engineer Skilled in Applying Expertise in the
-                Development of Systems
-              </h1>
-              {/* <Typewriter
-                  options={{ skipAddStyles: true, loop: true }}
-                  onInit={(typewriter) => {
-                    typewriter
-                      .pauseFor(1000)
-                      .changeDeleteSpeed(5000)
-                      .typeString("SOFTWARE ENGINEER")
-                      .pauseFor(1500)
-                      .deleteAll()
-                      .pauseFor(1500)
-                      .typeString("UX/UI Designer")
-                      .pauseFor(1500)
-                      .deleteAll()
-                      .pauseFor(1500)
-                      .typeString("NETWORK ENGINEER")
-                      .pauseFor(1500)
-                      .deleteAll()
-                      .typeString("")
-                      .start();
-                  }}
-                /> */}
-            </center>
-          </div>
-        </main>
-
-        {/* <div className="absolute bottom-2 w-full z-10">
+          {/* <div className="flex flex-row align-center items-center justify-center gap-8 h-1/2"> */}
           <center>
-            <GlassButton
-              playing={playing}
-              displayImageButton="true"
-              isPlaying={handleValueChange}
-              border={false}
-            />
+            <h1 className="text-white text-5xl w-[1000px]">
+              Software Engineer Skilled in Applying Expertise in the Development
+              of Systems
+            </h1>
           </center>
-        </div> */}
+          {/* </div> */}
+        </main>
       </div>
     </>
   );
